93f193d5ef74505819682bd1187de8343f07687c
Antony Yang <mrant0ny@gmail.com>
2015-06-14 08:58:30 -0700

    working prototype

---
+++ SFOmon/MonsterView.swift
@@ -0,0 +1,38 @@
+//
+//  MonsterView.swift
+//  SFOmon
+//
+//  Created by Antony Yang on 6/14/15.
+//  Copyright (c) 2015 Antony Yang. All rights reserved.
+//
+
+import UIKit
+
+class MonsterView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func done(sender: AnyObject) {
+        self.dismissViewControllerAnimated(false, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
